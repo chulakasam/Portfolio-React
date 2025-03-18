ac2078ee56e1a0c3d58d9e06b56b9f03b6aad400
chulakasam <sampathchulaka33872@gmail.com>
2025-03-18 08:37:25 +0530

    npm build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio-React/', // Add this line, replace 'your-repo-name' with your actual repo name
 })
